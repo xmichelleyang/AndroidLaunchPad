a9dbd6c9a837abbd8a2c281710f79e9df4cd73b9
Michelle Yang <zomgitsmichelle@gmail.com>
2017-07-26 21:54:22 -0700

    Functional map button.

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -4,17 +4,19 @@ document.addEventListener("deviceready", function() {
     cameraLink.addEventListener("click", function() {
         if (navigator.camera) {
             navigator.camera.getPicture(onCameraSuccess, onCameraFail, {
-                quality: 50, // range 0-100 with 100 being best; 50 is defaulta
-                sourceType: Camera.PictureSourceType.CAMERA,  // 0=Photo Library, 1=Camera, 2=Saved Album
+                quality: 50, // Quality from 1 - 100. 50 is default
+                sourceType: Camera.PictureSourceType.CAMERA,  // 0 = Photo Library, 1 = Camera, 2 = Saved Album
                 destinationType: Camera.DestinationType.FILE_URI,
-                saveToPhotoAlbum: true, // On Android, this is saved to the Pictures directory.
-                encodingType: 0  // 0=JPG, 1=PNG; 0 is default
+                saveToPhotoAlbum: true,
+                encodingType: 0  // 0 = JPG, 1 = PNG
             });
         } else {
             alert('Camera is not available.');
         }
     });
 
+
+    // TODO: THE STUDENTS HAVENT WRITTEN THIS YET
     function onCameraSuccess(uri) {
         // Args: message, callback when dialog is dismissed, title, button name
         navigator.notification.alert('Picture saved.', null, 'Camera', 'OK');
@@ -27,4 +29,27 @@ document.addEventListener("deviceready", function() {
             alert(msg);
         }
     }
+
+    // Map
+    var mapLink = document.querySelector("#map-link");
+    mapLink.addEventListener("click", function(){
+      alert("WTF");
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(onGeolocationSuccess, onGeolocationFail, {timeout: 3000});
+      } else {
+        alert("Unable to get current location");
+      }
+    });
+
+    function onGeolocationSuccess(position){
+      var latitude = position.coords.latitude;
+      var longitude = position.coords.longitude;
+
+      window.location = "geo: " + latitude + "," + longitude;
+    }
+
+    function onGeolocationFail(msg) {
+      alert(msg);
+    }
+
 });
