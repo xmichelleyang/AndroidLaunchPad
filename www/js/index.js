7031ae9ec9890a6ffac1cd6acdf70c09fa4b57f3
Michelle Yang <zomgitsmichelle@gmail.com>
2017-07-26 17:14:00 -0700

    Added cordova script tag and deviceready instead of DOMContent...

--- www/js/index.js
+++ www/js/index.js
@@ -1,16 +1,60 @@
-document.addEventListener('deviceready', function() {
-    
-    console.log("hello");
-    alert("Waht");
-    var camera = document.getElementById("camera-link");
-    
-    camera.addEventListener("click", function(){
-        alert("Helloo");
-        console.log("clicked");
-        
-        
+document.addEventListener("deviceready", function() {
+    // Camera
+    var cameraLink = document.querySelector('#camera-link');
+    cameraLink.addEventListener("click", function() {
+        if (navigator.camera) {
+            navigator.camera.getPicture(onCameraSuccess, onCameraFail, {
+                quality: 50, // range 0-100 with 100 being best; 50 is defaulta
+                sourceType: Camera.PictureSourceType.CAMERA,  // 0=Photo Library, 1=Camera, 2=Saved Album
+                destinationType: Camera.DestinationType.FILE_URI,
+                saveToPhotoAlbum: true, // On Android, this is saved to the Pictures directory.
+                encodingType: 0  // 0=JPG, 1=PNG; 0 is default
+            });
+        } else {
+            alert('Camera is not available.');
+        }
     });
-    
-    
-    
+
+    // Map
+    var mapLink = document.querySelector('#map-link');
+    mapLink.addEventListener("click", function() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(onGeolocationSuccess, onGeolocationFail, { timeout: 3000 });
+        } else {
+            alert('Unable to get current location.');
+        }
+    });
+
+
+    //
+    // Camera callbacks
+    //
+
+    function onCameraSuccess(uri) {
+        // Args: message, callback when dialog is dismissed, title, button name
+        navigator.notification.alert('Picture saved.', null, 'Camera', 'OK');
+    }
+
+    function onCameraFail(msg) {
+        // Ignore only if message is "Camera cancelled." which occurs if you
+        // cancel this operation.
+        if (msg.indexOf('cancel') == -1) {
+            alert(msg);
+        }
+    }
+
+
+    //
+    // Map-related callbacks
+    //
+
+    function onGeolocationSuccess(position) {
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+        window.location = "geo:" + latitude + "," + longitude;
+    }
+
+    function onGeolocationFail(msg) {
+        alert(msg);
+    }
 });
